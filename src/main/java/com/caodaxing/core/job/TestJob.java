841c2510f5174c8c5934d6ad7659444bd12760c8
daxing.cao <daxing.cao@ezhiyang.com>
2018-09-28 13:50:08 +0800

    spring自带定时任务注解使用

---
+++ src/main/java/com/caodaxing/core/job/TestJob.java
@@ -0,0 +1,19 @@
+package com.caodaxing.core.job;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@EnableScheduling
+@Component
+public class TestJob {
+	
+	private int num = 1;
+	
+	@Scheduled(cron="0-30/5 42 13 * * ?")
+	public void queryLoginInfo() {
+		System.out.println("第"+num+"调用...");
+		num++;
+	}
+
+}
